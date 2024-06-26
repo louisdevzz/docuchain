584a8303786cbc70618eeb1c4557a09df4d060a0
vo huu nhan <vohuunhan1310@gmail.com>
2024-06-26 16:55:00 +0700

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,300 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import React, { useState } from "react";
+import {
+  createSmartAccountClient,
+  BiconomySmartAccountV2,
+  PaymasterMode,
+} from "@biconomy/account";
+import { ethers } from "ethers";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
+import { contractABI } from "@/contract/contractABI";
+import { ABI } from "@/contract/ABI";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
+  const [smartAccount, setSmartAccount] =
+    useState<BiconomySmartAccountV2 | null>(null);
+  const [smartAccountAddress, setSmartAccountAddress] = useState<string | null>(
+    null
+  );
+  const [count, setCount] = useState<string | null>(null);
+  const [txnHash, setTxnHash] = useState<string | null>(null);
+  const [chainSelected, setChainSelected] = useState<number>(0);
+
+  const chains = [
+    {
+      chainId: 11155111,
+      name: "Ethereum Sepolia",
+      providerUrl: "https://eth-sepolia.public.blastapi.io",
+      incrementCountContractAdd: "0xd9ea570eF1378D7B52887cE0342721E164062f5f",
+      uniCertNFT: "0x53191dB16a946E5aC8205Dd1a49528730b92b5c0",
+      biconomyPaymasterApiKey: "pg90HOfS1.2b0110e8-eed6-41b3-a3c6-efc4742f148c",
+      explorerUrl: "https://sepolia.etherscan.io/tx/",
+    },
+    {
+      chainId: 80002,
+      name: "Polygon Amoy",
+      providerUrl: "https://rpc-amoy.polygon.technology/",
+      incrementCountContractAdd: "0xfeec89eC2afD503FF359487967D02285f7DaA9aD",
+      biconomyPaymasterApiKey: "TVDdBH-yz.5040805f-d795-4078-9fd1-b668b8817642",
+      explorerUrl: "https://www.oklink.com/amoy/tx/",
+    },
+  ];
+
+  const connect = async () => {
+    try {
+      const chainConfig =
+        chainSelected == 0
+          ? {
+              chainNamespace: CHAIN_NAMESPACES.EIP155,
+              chainId: "0xaa36a7",
+              rpcTarget: chains[chainSelected].providerUrl,
+              displayName: "Ethereum Sepolia",
+              blockExplorer: "https://sepolia.etherscan.io/",
+              ticker: "ETH",
+              tickerName: "Ethereum",
+            }
+          : {
+              chainNamespace: CHAIN_NAMESPACES.EIP155,
+              chainId: "0x13882",
+              rpcTarget: chains[chainSelected].providerUrl,
+              displayName: "Polygon Amoy",
+              blockExplorer: "https://www.oklink.com/amoy/",
+              ticker: "MATIC",
+              tickerName: "Polygon Matic",
+            };
+
+      //Creating web3auth instance
+      const web3auth = new Web3Auth({
+        clientId:
+          "BF7VNWZp3xcReMTWGjVyaDIaroawluipOGC9Kfrhq1FgNK_cx1G1Dhxnt3skEH6ZYrODKXhUGJEA0rVCAXbublo", // Get your Client ID from the Web3Auth Dashboard https://dashboard.web3auth.io/
+        web3AuthNetwork: "sapphire_devnet", // Web3Auth Network
+        chainConfig,
+        uiConfig: {
+          appName: "UniCert On-Chain",
+          mode: "dark", // light, dark or auto
+          loginMethodsOrder: ["apple", "google", "twitter"],
+          logoLight: "https://web3auth.io/images/web3auth-logo.svg",
+          logoDark: "https://web3auth.io/images/web3auth-logo---Dark.svg",
+          defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+          loginGridCol: 3,
+          primaryButton: "socialLogin", // "externalLogin" | "socialLogin" | "emailLogin"
+        },
+      });
+
+      await web3auth.initModal();
+      const web3authProvider = await web3auth.connect();
+      const ethersProvider = new ethers.providers.Web3Provider(
+        web3authProvider as any
+      );
+      const web3AuthSigner = ethersProvider.getSigner();
+
+      const config = {
+        biconomyPaymasterApiKey: chains[chainSelected].biconomyPaymasterApiKey,
+        bundlerUrl: `https://bundler.biconomy.io/api/v2/${chains[chainSelected].chainId}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`, // <-- Read about this at https://docs.biconomy.io/dashboard#bundler-url
+      };
+
+      const smartWallet = await createSmartAccountClient({
+        signer: web3AuthSigner,
+        biconomyPaymasterApiKey: config.biconomyPaymasterApiKey,
+        bundlerUrl: config.bundlerUrl,
+        rpcUrl: chains[chainSelected].providerUrl,
+        chainId: chains[chainSelected].chainId,
+      });
+
+      console.log("Biconomy Smart Account", smartWallet);
+      setSmartAccount(smartWallet);
+      const saAddress = await smartWallet.getAccountAddress();
+      console.log("Smart Account Address", saAddress);
+      setSmartAccountAddress(saAddress);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getCountId = async () => {
+    const contractAddress = chains[chainSelected].incrementCountContractAdd;
+    const provider = new ethers.providers.JsonRpcProvider(
+      chains[chainSelected].providerUrl
+    );
+    const contractInstance = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      provider
+    );
+    const countId = await contractInstance.getCount();
+    setCount(countId.toString());
+  };
+
+  const addUNI = async()=>{
+    try{
+      const toastId = toast("Populating Transaction", { autoClose: false });
+
+      const contractAddress = chains[chainSelected].uniCertNFT;
+      const provider = new ethers.providers.JsonRpcProvider(
+        chains[chainSelected].providerUrl
+      );
+      const contractInstance = new ethers.Contract(
+        contractAddress as string,
+        ABI,
+        provider
+      );
+      const minTx = await contractInstance.populateTransaction.safeMint(smartAccountAddress,"https://gateway.pinata.cloud/ipfs/QmRhjAKMDzTr4StFJPAs5jQYhK8UiDCFo69oLpTrZqrquv");
+      console.log("Mint Tx Data", minTx.data);
+      const tx1 = {
+        to: contractAddress,
+        data: minTx.data,
+      };
+      toast.update(toastId, {
+        render: "Sending Transaction",
+        autoClose: false,
+      });
+      //@ts-ignore
+      const userOpResponse = await smartAccount?.sendTransaction(tx1, {
+        paymasterServiceData: { mode: PaymasterMode.SPONSORED },
+      });
+      //@ts-ignore
+      const { transactionHash } = await userOpResponse.waitForTxHash();
+      console.log("Transaction Hash", transactionHash);
+
+      if (transactionHash) {
+        toast.update(toastId, {
+          render: "Transaction Successful",
+          type: "success",
+          autoClose: 5000,
+        });
+        setTxnHash(transactionHash);
+        await getCountId();
+      }
+    }catch(error){
+      console.log("error",error)
+    }
+  }
+
+  const incrementCount = async () => {
+    try {
+      const toastId = toast("Populating Transaction", { autoClose: false });
+
+      const contractAddress = chains[chainSelected].incrementCountContractAdd;
+      const provider = new ethers.providers.JsonRpcProvider(
+        chains[chainSelected].providerUrl
+      );
+      const contractInstance = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        provider
+      );
+      const minTx = await contractInstance.populateTransaction.increment();
+      console.log("Mint Tx Data", minTx.data);
+      const tx1 = {
+        to: contractAddress,
+        data: minTx.data,
+      };
+
+      toast.update(toastId, {
+        render: "Sending Transaction",
+        autoClose: false,
+      });
+      //@ts-ignore
+      const userOpResponse = await smartAccount?.sendTransaction(tx1, {
+        paymasterServiceData: { mode: PaymasterMode.SPONSORED },
+      });
+      //@ts-ignore
+      const { transactionHash } = await userOpResponse.waitForTxHash();
+      console.log("Transaction Hash", transactionHash);
+
+      if (transactionHash) {
+        toast.update(toastId, {
+          render: "Transaction Successful",
+          type: "success",
+          autoClose: 5000,
+        });
+        setTxnHash(transactionHash);
+        await getCountId();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Transaction Unsuccessful", { autoClose: 5000 });
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center justify-start gap-8 p-24">
+      <div className="text-[4rem] font-bold text-orange-400">
+        UniCert On-Chain
+      </div>
+      {!smartAccount && (
+        <>
+          <div className="flex flex-row justify-center items-center gap-4">
+            <div
+              className={`w-[8rem] h-[3rem] cursor-pointer rounded-lg flex flex-row justify-center items-center text-white ${
+                chainSelected == 0 ? "bg-orange-600" : "bg-black"
+              } border-2 border-solid border-orange-400`}
+              onClick={() => {
+                setChainSelected(0);
+              }}
+            >
+              Eth Sepolia
+            </div>
+            <div
+              className={`w-[8rem] h-[3rem] cursor-pointer rounded-lg flex flex-row justify-center items-center text-white ${
+                chainSelected == 1 ? "bg-orange-600" : "bg-black"
+              } bg-black border-2 border-solid border-orange-400`}
+              onClick={() => {
+                setChainSelected(1);
+              }}
+            >
+              Poly Amoy
+            </div>
+          </div>
+          <button
+            className="w-[10rem] h-[3rem] bg-orange-300 text-black font-bold rounded-lg"
+            onClick={connect}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            Web3Auth Sign in
+          </button>
+        </>
+      )}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {smartAccount && (
+        <>
+          {" "}
+          <span>Smart Account Address</span>
+          <span>{smartAccountAddress}</span>
+          <span>Network: {chains[chainSelected].name}</span>
+          <div className="flex flex-row justify-between items-start gap-8">
+            <div className="flex flex-col justify-center items-center gap-4">
+              <button
+                className="w-[10rem] h-[3rem] bg-orange-300 text-black font-bold rounded-lg"
+                onClick={getCountId}
+              >
+                Get Count Id
+              </button>
+              <span>{count}</span>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-4">
+              <button
+                className="w-[10rem] h-[3rem] bg-orange-300 text-black font-bold rounded-lg"
+                onClick={addUNI}
+              >
+                Mint NFT
+              </button>
+              {txnHash && (
+                <a
+                  target="_blank"
+                  href={`${chains[chainSelected].explorerUrl + txnHash}`}
+                >
+                  <span className="text-white font-bold underline">
+                    Txn Hash
+                  </span>
+                </a>
+              )}
+            </div>
+          </div>
+          <span className="text-white">Open console to view console logs.</span>
+        </>
+      )}
     </main>
   );
 }
